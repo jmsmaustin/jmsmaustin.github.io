b8e0e0be065b3e574d19fc8f00c8472884516c83
Mitchell Austin <jmaustin@ou.edu>
2019-04-25 14:37:35 -0500

    Big Things Happening

--- AboutMeWeb/AboutMeWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AboutMeWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- AboutMeWeb/AboutMeWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AboutMeWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
